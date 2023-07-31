c58bc036c80786fb95161d1ea6a9f223b627bce7
sanskar021 <sanskarchaurey2000@gmail.com>
2023-08-01 02:12:40 +0530

    feat: My first feature

---
+++ models/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/snapchat")
+    .then(() => console.log("connected!"))
+    .catch(err => console.log(err.message))
+    
